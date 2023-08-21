e653a0dc9d6f755de4e196e319ca154078859cd2
LibertyReman <libertyreman@gmail.com>
2023-08-22 07:39:57 +0900

    検索フォームのWordPress化

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
-          <form action="./archive_search.html" class="c-form p-search"> <!-- flex row ヨコ並び -->
-            <input type="text" placeholder="" class="c-form__text p-search__text">
-            <input type="submit" value="検索" class="c-form__submit p-search__submit">
+          <form class="c-form p-search" id="searchform" method="get" action="<?php echo home_url('/'); ?>"> <!-- flex row ヨコ並び -->
+            <input class="c-form__text p-search__text" type="text" placeholder="" name="s" id="s">
+            <input class="c-form__submit p-search__submit" type="submit" value="検索" >
           </form>
 
