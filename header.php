3bb8e305b79eec2bfd4839d20c9b19053f337003
LibertyReman <libertyreman@gmail.com>
2023-08-22 07:13:07 +0900

    テンプレートタグを使用してheader.phpを編集

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta chaarset="utf-8">
     <meta name="viewport" content="width=width=device-width, initial-scale=1.0">
     <link rel="icon" href="./img/favicon.ico">
-    <meta name="description" content="サイトの説明文が入ります">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
     <title>ハンバーガーサイト</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -13,7 +13,7 @@
     <link href="./css/style.css" rel="stylesheet">
   </head>
 
-  <body>
+  <body <?php body_class(); ?>> <!-- 表示するページの種類によってclassを付与 -->
     <main class="l-main">
       <aside>
         <?php get_sidebar(); ?> <!-- sidebar.phpを読み込むテンプレートタグ -->
@@ -21,7 +21,7 @@
 
       <div class="l-main__body">
         <header class="l-header"> <!-- flex column center タテ並び真ん中 -->
-          <h1 class="l-header__logo"><a href="./index.html">Hamburger</a></h1>
+          <h1 class="l-header__logo"><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
 
           <form action="./archive_search.html" class="c-form p-search"> <!-- flex row ヨコ並び -->
             <input type="text" placeholder="" class="c-form__text p-search__text">
