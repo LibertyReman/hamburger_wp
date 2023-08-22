cefa3ae47fce75931cd5bcd22c6e317027346833
LibertyReman <libertyreman@gmail.com>
2023-08-23 06:19:05 +0900

    ナビゲーションの追加

--- sidebar.php
+++ sidebar.php
@@ -11,43 +11,8 @@
 
             <h2  class="p-gmenu__content__title">Menu</h2>
 
-            <ul>
-              <li>
-                <a href="/">バーガー</a>
-                <ul>
-                  <li><a href="/">ハンバーガー</a></li>
-                  <li><a href="./archive.html">チーズバーガー</a></li>
-                  <li><a href="/">テリヤキバーガー</a></li>
-                  <li><a href="/">アボカドバーガー</a></li>
-                  <li><a href="/">フィッシュバーガー</a></li>
-                  <li><a href="/">ベーコンバーガー</a></li>
-                  <li><a href="/">チキンバーガー</a></li>
-                </ul>
-              </li>
+            <?php wp_nav_menu(); ?>
 
-              <li>
-                <a href="/">サイド</a>
-                <ul>
-                  <li><a href="/">ポテト</a></li>
-                  <li><a href="/">サラダ</a></li>
-                  <li><a href="/">ナゲット</a></li>
-                  <li><a href="/">コーン</a></li>
-                </ul>
-              </li>
-
-              <li>
-                <a href="/">ドリンク</a>
-                <ul>
-                  <li><a href="/">コーラ</a></li>
-                  <li><a href="/">ファンタ</a></li>
-                  <li><a href="/">オレンジ</a></li>
-                  <li><a href="/">アップル</a></li>
-                  <li><a href="/">紅茶（Ice/Hot）</a></li>
-                  <li><a href="/">コーヒー（Ice/Hot）</a></li>
-                </ul>
-              </li>
-
-            </ul>
           </div> <!-- .p-gmenu__content -->
         </div> <!-- .p-gmenu -->
 
