d088c189d1b515e1f15fc6bff16e3f03490bed1a
LibertyReman <libertyreman@gmail.com>
2023-08-28 05:36:00 +0900

    投稿データ一覧を表示

--- archive.php
+++ archive.php
@@ -1,89 +1,9 @@
-<!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta chaarset="utf-8">
-    <meta name="viewport" content="width=width=device-width, initial-scale=1.0">
-    <link rel="icon" href="./img/favicon.ico">
-    <meta name="description" content="サイトの説明文が入ります">
-    <title>ハンバーガーサイト</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-    <link href="./css/ress.css" rel="stylesheet">
-    <link href="./css/style.css" rel="stylesheet">
-  </head>
 
-  <body>
-    <main class="l-main">
-      <aside>
-        <div class="p-gmenu"> <!-- flex row flex-end ヨコ並び右端 -->
-          <button class="p-gmenu__menu-opbtn js-menu-button">
-            <span>Menu</span>
-          </button>
-
-          <div class="p-gmenu__layer"></div>
-          <div class="p-gmenu__content"> <!-- flex column flex-end タテ並び右端 -->
-            <button class="p-gmenu__content__menu-clbtn js-menu-button">
-              <span>Close</span>
-            </button>
-
-            <h2  class="p-gmenu__content__title">Menu</h2>
-
-            <ul>
-              <li>
-                <a href="/">バーガー</a>
-                <ul>
-                  <li><a href="/">ハンバーガー</a></li>
-                  <li><a href="./archive.html">チーズバーガー</a></li>
-                  <li><a href="/">テリヤキバーガー</a></li>
-                  <li><a href="/">アボカドバーガー</a></li>
-                  <li><a href="/">フィッシュバーガー</a></li>
-                  <li><a href="/">ベーコンバーガー</a></li>
-                  <li><a href="/">チキンバーガー</a></li>
-                </ul>
-              </li>
-
-              <li>
-                <a href="/">サイド</a>
-                <ul>
-                  <li><a href="/">ポテト</a></li>
-                  <li><a href="/">サラダ</a></li>
-                  <li><a href="/">ナゲット</a></li>
-                  <li><a href="/">コーン</a></li>
-                </ul>
-              </li>
-
-              <li>
-                <a href="/">ドリンク</a>
-                <ul>
-                  <li><a href="/">コーラ</a></li>
-                  <li><a href="/">ファンタ</a></li>
-                  <li><a href="/">オレンジ</a></li>
-                  <li><a href="/">アップル</a></li>
-                  <li><a href="/">紅茶（Ice/Hot）</a></li>
-                  <li><a href="/">コーヒー（Ice/Hot）</a></li>
-                </ul>
-              </li>
-
-            </ul>
-          </div> <!-- .p-gmenu__content -->
-        </div> <!-- .p-gmenu -->
-      </aside>
-
-      <div class="l-main__body">
-        <header class="l-header"> <!-- flex column center タテ並び真ん中 -->
-          <h1 class="l-header__logo"><a href="./index.html">Hamburger</a></h1>
-
-          <form action="./archive_search.html" class="c-form p-search"> <!-- flex row ヨコ並び -->
-            <input type="text" placeholder="" class="c-form__text p-search__text">
-            <input type="submit" value="検索" class="c-form__submit p-search__submit">
-          </form>
-        </header>
-
+<?php get_header(); ?> <!-- header.phpを読み込むテンプレートタグ -->
         <article>
           <div class="p-mainvisual p-mainvisual--img-archive"> <!-- flex column center center タテヨコ真ん中 -->
             <h1 class="p-mainvisual__title">Menu:</h1>
-            <p class="p-mainvisual__subtitle">チーズバーガー</p>
+            <p class="p-mainvisual__subtitle"><?php single_cat_title(); ?></p> <!-- カテゴリー名を表示 -->
           </div>
 
           <div class="l-main__wrapper">
@@ -93,35 +13,21 @@
             </div>
 
             <ul>
-              <li class="p-card">
-                <div class="p-card__img"></div>
-                <div class="p-card__content">
-                  <h3 class="p-card__content__title">チーズバーガー</h3>
-                  <h4 class="p-card__content__subtitle">小見出しが入ります</h4>
-                  <p class="p-card__content__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                  <button onclick="location.href='./single.html'" class="c-button p-card__content__button">詳しく見る</button>
-                </div>
-              </li>
-
-              <li class="p-card">
-                <div class="p-card__img"></div>
-                <div class="p-card__content">
-                  <h3 class="p-card__content__title">ダブルチーズバーガー</h3>
-                  <h4 class="p-card__content__subtitle">小見出しが入ります</h4>
-                  <p class="p-card__content__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                  <button class="c-button p-card__content__button">詳しく見る</button>
-                </div>
-              </li>
-
-              <li class="p-card">
-                <div class="p-card__img"></div>
-                <div class="p-card__content">
-                  <h3 class="p-card__content__title">スペシャルチーズバーガー</h3>
-                  <h4 class="p-card__content__subtitle">小見出しが入ります</h4>
-                  <p class="p-card__content__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                  <button class="c-button p-card__content__button">詳しく見る</button>
-                </div>
-              </li>
+              <?php if(have_posts()) : // 投稿データがあるか確認
+                while(have_posts()) :
+                  the_post(); // 投稿データの取得 ?>
+                  <li class="p-card">
+                    <div class="p-card__img"><?php the_post_thumbnail(); ?></div> <!-- アイキャッチ画像を出力 -->
+                    <div class="p-card__content">
+                      <h3 class="p-card__content__title"><?php the_title(); ?></h3>
+                      <div class="p-card__content__text"><?php the_content('...'); ?></div>
+                      <button onclick="location.href='<?php the_permalink(); ?>'" class="c-button p-card__content__button">詳しく見る</button>
+                    </div>
+                  </li>
+                <?php endwhile;
+              else : // 投稿データがない場合 ?>
+                <p>表示する投稿データがありません</p>
+              <?php endif; ?>
             </ul>
 
             <section class="p-pagenavigation"> <!-- flex row space-between ヨコ並び両端から均等に配置 -->
@@ -146,19 +52,6 @@
       </div> <!-- .l-main__body -->
 
     </main>
-
-    <footer class="l-footer">
-      <ul class="l-footer__info"> <!-- flex row center ヨコ並び真ん中-->
-        <li class="l-footer__info__list"><a href="./page.html">ショップ情報</a></li>
-        <li class="l-footer__info__list"><a href="./page.html">ヒストリー</a></li>
-      </ul>
-
-      <p class="p-copyright">Copyright: RaiseTech</p> <!-- flex row center ヨコ並び真ん中-->
-    </footer>
-    <script src="./js/jquery-3.4.1.min.js"></script>
-    <script src="./js/menu-button.js"></script>
-  </body>
-
-</html>
+<?php get_footer(); ?> <!-- footer.phpを読み込むテンプレートタグ -->
 
 
