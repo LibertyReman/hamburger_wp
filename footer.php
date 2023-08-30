571c36ad9dd5efa03b00892254257121003284b2
LibertyReman <libertyreman@gmail.com>
2023-08-31 05:40:54 +0900

    固定ページの表示

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     <footer class="l-footer">
       <ul class="l-footer__info"> <!-- flex row center ヨコ並び真ん中-->
-        <li class="l-footer__info__list"><a href="./page.html">ショップ情報</a></li>
-        <li class="l-footer__info__list"><a href="./page.html">ヒストリー</a></li>
+        <li class="l-footer__info__list"><a href="<?php echo esc_url(home_url('/ショップについて'));?>">ショップ情報</a></li>
+        <li class="l-footer__info__list"><a href="<?php echo esc_url(home_url('/ヒストリー'));?>">ヒストリー</a></li>
       </ul>
 
       <p class="p-copyright">Copyright: RaiseTech</p> <!-- flex row center ヨコ並び真ん中-->
