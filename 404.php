971a17b51a8a7cd5d93763d0ca6ced8d82e83775
LibertyReman <libertyreman@gmail.com>
2023-09-01 23:38:31 +0900

    404ページの追加

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?> <!-- header.phpを読み込むテンプレートタグ -->
+        <article>
+
+          <div class="p-error">
+            <h1 class="p-error__title">404</h1>
+            <p class="p-error__text">Not Found</p>
+            <p class="p-error__text">お探しのページは見つかりませんでした。</p>
+          </div>
+
+        </article>
+      </div> <!-- .l-main__body -->
+
+    </main>
+<?php get_footer(); ?> <!-- footer.phpを読み込むテンプレートタグ -->
+
