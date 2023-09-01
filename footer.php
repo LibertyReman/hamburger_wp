a6e35237ae8b9286a4462cf464701c8661c99b51
LibertyReman <libertyreman@gmail.com>
2023-09-02 04:29:10 +0900

    固定ページの表示をナビゲーションに変更

--- footer.php
+++ footer.php
@@ -1,9 +1,5 @@
     <footer class="l-footer">
-      <ul class="l-footer__info"> <!-- flex row center ヨコ並び真ん中-->
-        <li class="l-footer__info__list"><a href="<?php echo esc_url(home_url('/ショップについて'));?>">ショップ情報</a></li>
-        <li class="l-footer__info__list"><a href="<?php echo esc_url(home_url('/ヒストリー'));?>">ヒストリー</a></li>
-      </ul>
-
+      <?php wp_nav_menu(array('menu' => 'footermenu')); ?> <!-- ナビゲーションメニューの表示 -->
       <p class="p-copyright">Copyright: RaiseTech</p> <!-- flex row center ヨコ並び真ん中-->
     </footer>
     <?php wp_footer(); ?> <!-- 必ず入れる プラグインや情報出力が機能しなくなってしまう -->
