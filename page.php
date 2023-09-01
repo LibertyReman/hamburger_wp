a140e43d2c91231485f16e97acc5afa171481971
LibertyReman <libertyreman@gmail.com>
2023-09-02 04:46:04 +0900

    固定ページの表示をループ処理内に変更

--- page.php
+++ page.php
@@ -1,16 +1,23 @@
 <?php get_header(); ?> <!-- header.phpを読み込むテンプレートタグ -->
         <article>
-          <div class="p-mainvisual p-mainvisual--img-page"> <!-- flex column center center タテヨコ真ん中 -->
-            <h1 class="p-mainvisual__title"><?php echo get_the_title(); ?></h1>
-          </div>
+          <?php if(have_posts()) : // 投稿データがあるか確認
+            while(have_posts()) :
+              the_post(); // 投稿データの取得 ?>
+              <div class="p-mainvisual p-mainvisual--img-page"> <!-- flex column center center タテヨコ真ん中 -->
+                <h1 class="p-mainvisual__title"><?php the_title(); ?></h1>
+              </div>
 
-          <div class="l-main__wrapper">
-            <div class="p-sentence">
+              <div class="l-main__wrapper">
+                <div class="p-sentence">
 
-              <?php the_content(); ?> <!-- 本文の出力 -->
+                  <?php the_content(); ?> <!-- 本文の出力 -->
 
-            </div> <!-- .p-sentence -->
-          </div> <!-- .l-main__wrapper -->
+                </div> <!-- .p-sentence -->
+              </div> <!-- .l-main__wrapper -->
+            <?php endwhile;
+          else : // 投稿データがない場合 ?>
+            <p>表示する投稿データがありません</p>
+          <?php endif; ?>
         </article>
       </div> <!-- .l-main__body -->
 
