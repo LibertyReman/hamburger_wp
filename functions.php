5955cc05198342bca53827b1e33029337685027e
LibertyReman <libertyreman@gmail.com>
2023-12-03 04:38:12 +0900

    カスタム投稿タイプ「ニュース」とカスタムタクソノミーアーカイブページの追加

--- functions.php
+++ functions.php
@@ -69,6 +69,51 @@
   add_action( 'save_post', 'save_custom_fields' );
 
 
+  // カスタム投稿タイプ ニュース を登録
+  add_action('init', 'create_post_type');
+
+  function create_post_type() {
+    register_post_type(
+      'news',
+      array(
+        'label' => 'ニュース',
+        'public' => true,
+        'has_archive' => true,
+        'show_in_rest' => true,
+        'menu_position' => 5,
+        'supports' => array(
+          'title',
+          'editor',
+          'thumbnail',
+          'revisions',
+        ),
+      )
+    );
+
+    register_taxonomy(
+      'news-cat',
+      'news',
+      array(
+        'label' => 'カテゴリー',
+        'hierarchical' => true,
+        'public' => true,
+        'show_in_rest' => true,
+      )
+    );
+
+    register_taxonomy(
+      'news-tag',
+      'news',
+      array(
+        'label' => 'タグ',
+        'hierarchical' => false,
+        'public' => true,
+        'show_in_rest' => true,
+        'update_count_callback' => '_update_post_term_count',
+      )
+    );
+  }
+
   // アーカイブページに属する記事数を取得
   function get_postno_from_archive() {
     $term = get_queried_object();
---
+++ taxonomy-news-tag.php
@@ -0,0 +1,80 @@
+<?php get_header(); ?> <!-- header.phpを読み込むテンプレートタグ -->
+        <article>
+          <div class="p-mainvisual p-mainvisual--img-archive"> <!-- flex column center center タテヨコ真ん中 -->
+            <h1 class="p-mainvisual__title">News:</h1>
+            <p class="p-mainvisual__subtitle"><?php echo single_cat_title('', false), "（", $wp_query->found_posts, "件）"; ?></p> <!-- カテゴリー名を表示 -->
+          </div>
+
+          <div class="l-main__wrapper">
+            <div class="p-sentence">
+              <h2 class="p-sentence__title">小見出しが入ります</h2>
+              <p class="p-sentence__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+            </div>
+
+            <ul>
+              <?php if(have_posts()) : // 投稿データがあるか確認
+                while(have_posts()) :
+                  the_post(); // 投稿データの取得 ?>
+                  <li class="p-card">
+                    <div class="p-card__img p-card__img--news"><img src="<?php bloginfo('template_directory'); ?>/img/news.jpg" alt="news icon"></div> <!-- アイキャッチ画像を出力 -->
+                    <div class="p-card__content p-card__content--news">
+                      <?php
+                        $days = 14;
+                        $today = date_i18n('U');  // 現在の時間
+                        $post_day = get_the_time('U');  // 投稿日の時間
+                        $term = date('U',($today - $post_day)) / 86400;
+                        if( $days > $term ){
+                          echo '<span class="p-card__content__mark-new">NEW</span>';
+                        }
+                      ?>
+                      <h3 class="p-card__content__title"><?php the_title(); ?></h3>
+                      <p class="p-card__content__cat"><?php echo get_the_term_list($post->ID, 'news-cat', '', ',&nbsp&nbsp'); ?></P>
+                      <p class="p-card__content__tag"><?php echo get_the_term_list($post->ID, 'news-tag'); ?></P>
+                      <div class="p-card__content__text">
+                        <?php
+                          $content = strip_tags($post->post_content);
+                          $max_strlen = 150;
+                          if (mb_strlen($content, 'UTF-8') > $max_strlen) {
+                            $content = mb_substr($content, 0, $max_strlen, 'UTF-8');
+                            echo $content . '…';
+                          } else {
+                            echo $content;
+                          }
+                        ?>
+                      </div>
+                      <button onclick="location.href='<?php the_permalink(); ?>'" class="c-button p-card__content__button p-card__content__button--suggest">詳しく見る</button>
+                    </div>
+                  </li>
+                <?php endwhile;
+              else : // 投稿データがない場合 ?>
+                <p>表示する投稿データがありません</p>
+              <?php endif; ?>
+            </ul>
+
+            <section class="p-pagenavigation"> <!-- flex row space-between ヨコ並び両端から均等に配置 -->
+              <?php
+                $current_pgae = get_query_var('paged');
+                $current_pgae = $current_pgae == 0 ? '1' : $current_pgae;
+                $max_pages = $wp_query->max_num_pages;
+                echo '<p class="p-pagenavigation__info">page '.$current_pgae .'/'.$max_pages .'</p>';
+              ?>
+
+              <?php the_posts_pagination(
+                array(
+                    'show_all'   => true, // 全てのページを表示
+                    'prev_next'  => true, // 「前へ」「次へ」のリンクを表示
+                    'prev_text'  => __( '&lt;&lt; '), // 「前へ」リンクのテキスト
+                    'next_text'  => __( ' &gt;&gt;'), // 「次へ」リンクのテキスト
+                    'type'       => 'list', // 戻り値の指定 (plain/list)
+                )
+              ); ?>
+            </section>
+
+          </div> <!-- .l-main__wrapper -->
+        </article>
+      </div> <!-- .l-main__body -->
+
+    </main>
+<?php get_footer(); ?> <!-- footer.phpを読み込むテンプレートタグ -->
+
+
